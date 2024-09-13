c7af3cf6ac279457d4fa899d8983ca77d9614745
Erick <erickkan2004@gmail.com>
2024-09-13 15:52:02 -0300

    overrides packagejson

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './weather.svg';
 
 export default function Weather() {
+
+    const [city, setCity] = useState();
+    const handleCityChange = (event) => {
+        setCity(event.target.value);
+    }
+
+    const handleClick = () => {
+        
+    }
+
     return (
         <div className='weather-container'>
             <header>
-                <img src={logo} className='logo' />
+                <img src={logo} className='logo' alt='logo' />
                 <h1>Weather App</h1>
             </header>
 
             <main className='show-weather'>
-                
+                <div className='input-container'>
+                    <span>Cidade</span>
+                    <input type='text' placeholder='Nome da cidade' value={city} onChange={handleCityChange} />
+                    <div className='button-container'>
+                        <button onnClick={handleClick} >Verificar clima</button>
+                    </div>
+                </div>
             </main>
         </div>
     )
